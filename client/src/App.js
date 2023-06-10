87454614a124bf84b24c268d105a6aaabbb7126e
willcline613 <willrcline.atx@gmail.com>
2023-06-10 16:28:33 -0500

    all

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import NotFound from './pages/NotFound';
+import Home from "./pages/home/Home"
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-center align-center min-100-vh bg-primary">
+          <Routes>
+            <Route 
+              path="/" 
+              element={<Home />}
+            />
+            {/* <Route 
+              path="/game" 
+            /> */}
+            <Route 
+              path="*"
+              element={<NotFound />}
+            />
+          </Routes>
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
---
+++ client/src/pages/home/Home.js
@@ -0,0 +1,65 @@
+import {gsap} from 'gsap'
+// import { Link } from 'react-router-dom';
+// import { useQuery } from '@apollo/client';
+// import { QUERY_MATCHUPS } from '../utils/queries';
+
+// import "./css/home.css"
+{/* <script src="./js/gameboard.js"></script>
+<script src="./js/infographic.js"></script> */}
+// <title>Simulation Hopper</title>
+
+const Home = () => {
+
+  const animateTheIntroZoom = () => {
+    console.log('animateTheIntroZoom click event_________')
+    var tl = gsap.timeline();
+
+    tl.to("#infoButton", {
+      opacity: 0,
+      duration: 1
+    });
+
+    // tl.to(startGame.current, {
+    //   opacity: 0,
+    //   duration: 1
+    // });
+
+    tl.to("#handheldNintendo", {
+      duration: 3,
+      scale: 3,
+      opacity: 0,
+      onComplete: function () {
+        window.location.href = "/game";
+      }
+    });
+
+    gsap.to("#handheldNintendoContainer", {
+      duration: 3,
+      scale: 3,
+      opacity: .3,
+      onComplete: function () {
+        window.location.href = "/game";
+      }
+    });
+  };
+
+  return (
+    <div>
+      <header>
+        <h1>Simulation Hopper</h1>
+      </header>
+      <div id="handheldNintendoContainer">
+
+          {/* <div id="infoGraphic" style="display: none"></div> */}
+          <div id="startGame" onClick={animateTheIntroZoom}></div>
+
+          <div id="info"></div>
+
+      </div>
+
+  
+    </div>
+  );
+};
+
+export default Home;
