ae4534f981449ffb9430dcbe6dc5050883bcae9d
Valais Girl <Yevette@MacBook-Pro.local>
2023-06-10 19:25:28 -0700

    added character, items, inventory and user object types, queries and mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -23,6 +23,67 @@ const typeDefs = gql`
     createMatchup(tech1: String!, tech2: String!): Matchup
     createVote(_id: String!, techNum: Int!): Matchup
   }
+
+  type Character {
+    character_id: Int!
+    searchable_name: String!
+    full_name: String!
+    role: String!
+    bio: String!
+  }
+
+
+  type Query {
+    test: String!
+    biography(searchableName: String!): Character!
+  }
+
+  type Item {
+    item_id: Int!
+    searchable_item: String!
+    item_name: String!
+    description: String!
+  }
+
+  type Query {
+    test: String!
+    item(searchableItem: String!): Item!
+    items: [Item]!
+  }
+
+  type Inventory {
+    id: ID!
+    character: Character!
+    item: Item!
+  }
+
+  type Query {
+    test: String!
+    inventoryByCharacter(character: String!): [Inventory]!
+    inventory: [Inventory]!
+  }
+
+  type Mutation {
+    trade(item1: String!, item2: String!): Inventory!
+  }
+
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    password: String!
+  }
+
+  type Mutation {
+    createUser(username: String!, email: String!, password: String!): User!
+    loginUser(email: String!, password: String!): User!
+    logoutUser: Boolean!
+  }
+
+  schema {
+    query: Query
+    mutation: Mutation
+  }
 `;
 
 module.exports = typeDefs;
