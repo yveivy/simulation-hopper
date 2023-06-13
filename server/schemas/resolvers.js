820e266bbd356f88d885000449d0cdc2bf93a364
Joanna Seybold <126947675+clever-girl31@users.noreply.github.com>
2023-06-13 12:40:28 -0500

    alt DB/schema structure

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,110 +1,209 @@
 
-const { Characters, Items, Inventory, User} = require('../models');
+// const { Characters, Items, Inventory, User} = require('../models');
 
-const resolvers = {
-  Query: {
-    test: () => 'Test get route is working',
+// const resolvers = {
+//   Query: {
+//     test: () => 'Test get route is working',
 
 
-   inventoryByCharacter: async (_, { character }) => {
-      try {
-        const inventory = await Inventory.findAll({
-          attributes: { exclude: [ 'id', 'character_id', 'item_id']},
-          include: [
+//    inventoryByCharacter: async (_, { character }) => {
+//       try {
+//         const inventory = await Inventory.findAll({
+//           attributes: { exclude: [ 'id', 'character_id', 'item_id']},
+//           include: [
 
-          { 
-            model: Characters,
-            where: { searchable_name: character },
-            attributes: { exclude: ['character_id', 'searchable_name', 'full_name', 'role', 'bio'] }
-          },
+//           { 
+//             model: Characters,
+//             where: { searchable_name: character },
+//             attributes: { exclude: ['character_id', 'searchable_name', 'full_name', 'role', 'bio'] }
+//           },
 
-          {
-            model: Items,
-            attributes: { exclude: ['searchable_item'] },
+//           {
+//             model: Items,
+//             attributes: { exclude: ['searchable_item'] },
 
-          },
-        ],
+//           },
+//         ],
           
-        });
+//         });
 
-        return inventory; 
+//         return inventory; 
 
-      } catch (error) {
-        console.error(error);
-        throw new Error('Internal server error');
-      }
-    },
+//       } catch (error) {
+//         console.error(error);
+//         throw new Error('Internal server error');
+//       }
+//     },
 
    
 
 
 
-   inventory: async () => {
+//    inventory: async () => {
       
-   },
+//    },
 
-   biography: async (_, { searchableName }) => {
+//    biography: async (_, { searchableName }) => {
 
-   },
+//    },
 
-   item: async (_, { searchableItem }) => {
+//    item: async (_, { searchableItem }) => {
 
-   },
+//    },
 
-   items: async () => {
+//    items: async () => {
 
-   },
+//    },
 
-  },
+//   },
   
-  Mutation: {
-    testPut: () => 'Test put route is working',
+//   Mutation: {
+//     testPut: () => 'Test put route is working',
 
-    trade: async (_, { item1, item2}) => {
-      //logic to swap items and update the inventory and return updated inventory object
-    },
+//     trade: async (_, { item1, item2}) => {
+//       //logic to swap items and update the inventory and return updated inventory object
+//     },
 
-    createUser: async (_, { username, email, password}) => {
+//     createUser: async (_, { username, email, password}) => {
+//       try {
+//         const dbUserData = await User.create({ username, email, password});
+//         return dbUserData;
+//       } catch (err){
+//         console.log(err);
+//         throw new Error('Failed to create user')
+//       }
+//     },
+
+//     loginUser: async (_, { email, password }, { req}) => {
+//       try {
+//         const dbUserData = await User.findOne({ where: { email} });
+
+//         if (!dbUserData) {
+//           throw new Error('Incorrect email or password. Please try again!');
+//         }
+
+//         const validPassword = await dbUserData.checkPassword(password);
+
+//         if (!validPassword) {
+//           throw new Error('Incorrect email or password. Please try again!');
+//         }
+
+//         req.session.loggedIn = true;
+//         return dbUserData;
+//       } catch (err) {
+//         console.log(err);
+//         throw new Error('Failed to login user');
+//       }
+//     },
+
+//     logoutUser: (_, __, { req }) => {
+//       if (req.session.loggedIn) {
+//         req.session.destroy();
+//         return true;
+//       } else {
+//         return false;
+//       }
+//     },
+//   },
+
+// };
+
+// module.exports = resolvers;
+
+
+
+
+// =======================
+// comment out everything above this line to use J resolvers
+
+const { client, userinfo } = require('../config/db')
+
+
+
+const resolvers = {
+  Query: {
+    userSaveFile: async () => {
+        const database = client.db('testingGameDb');
+        const collection = database.collection(userinfo.username);
       try {
-        const dbUserData = await User.create({ username, email, password});
-        return dbUserData;
-      } catch (err){
-        console.log(err);
-        throw new Error('Failed to create user')
+        const dbData = await collection.findOne(); // Retrieve the data from the collection
+        return dbData;
+      } catch (error) {
+        console.error('Failed to retrieve data from the database:', error);
+        return null;
       }
-    },
-
-    loginUser: async (_, { email, password }, { req}) => {
-      try {
-        const dbUserData = await User.findOne({ where: { email} });
-
-        if (!dbUserData) {
-          throw new Error('Incorrect email or password. Please try again!');
-        }
-
-        const validPassword = await dbUserData.checkPassword(password);
-
-        if (!validPassword) {
-          throw new Error('Incorrect email or password. Please try again!');
-        }
-
-        req.session.loggedIn = true;
-        return dbUserData;
-      } catch (err) {
-        console.log(err);
-        throw new Error('Failed to login user');
-      }
-    },
-
-    logoutUser: (_, __, { req }) => {
-      if (req.session.loggedIn) {
-        req.session.destroy();
-        return true;
-      } else {
-        return false;
-      }
-    },
+    }
   },
+  Character: {
+    abe: (parent) => {
+      return parent.abe;
+    },
+    barf: (parent) => {
+      return parent.barf;
+    },
+    hydra: (parent) => {
+      return parent.hydra;
+    },
+    shady: (parent) => {
+      return parent.shady;
+    },
+    taylor: (parent) => {
+      return parent.taylor
+    },
+    violet: (parent) => {
+      return parent.violet
+    },
+    zara: (parent) => {
+      return parent.zara
+    }
+  },
+  CharacterObject: {
+    detail: (characterObject) => {
+      return characterObject.detail
+    },
+    inventory: (characterObject) => {
+      const inventory = characterObject.inventory
+      return Object.keys(inventory).map((key) => ({
+        key,
+        ...inventory[key],
+      }));
+    },
+    wishlist: (characterObject) => {
+      return characterObject.wishlist
+    }
+  },
+  InventoryContainer: {
+    allItems: (inventoryItem) => {
+      return inventoryItem;
+    }
+  },
+  InventoryItem: {
+    key: (inventoryObject) => {
+      return inventoryObject.key
+    },
+    description: (inventoryObject) => {
+      return inventoryObject.description
+    },
+    name: (inventoryObject) => {
+      return inventoryObject.name
+    }
+  },
+  CharacterDetail: {
+    bio: (characterDetail) => {
+      console.log(characterDetail);
+      return characterDetail.bio
+    },
+    name: (characterDetail) => {
+      return characterDetail.name
+    },
+    role: (characterDetail) => {
+      return characterDetail.role
+    }
+  },
+  Userinfo: {
+    username: (parent) => parent.username,
+    password: (parent) => parent.password,
+  },  
 
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,66 +1,129 @@
+// const { gql } = require('apollo-server-express');
+
+// const typeDefs = gql`
+
+//   type Character {
+//     character_id: Int!
+//     searchable_name: String!
+//     full_name: String!
+//     role: String!
+//     bio: String!
+//   }
+
+
+//   type Query {
+//     test: String!
+//     biography(searchableName: String!): Character!
+//   }
+
+//   type Item {
+//     item_id: Int!
+//     searchable_item: String!
+//     item_name: String!
+//     description: String!
+//   }
+
+//   type Query {
+//     test: String!
+//     item(searchableItem: String!): Item!
+//     items: [Item]!
+//   }
+
+//   type Inventory {
+//     full_name: Character!
+//     item_name: Item!
+//   }
+
+//   type Query {
+//     test: String!
+//     inventoryByCharacter(character: String!): [Inventory]!
+//     inventory: [Inventory]!
+//   }
+
+//   type Mutation {
+//     trade(item1: String!, item2: String!): Inventory!
+//   }
+
+//   type User {
+//     id: ID!
+//     username: String!
+//     email: String!
+//     password: String!
+//   }
+
+//   type Mutation {
+//     createUser(username: String!, email: String!, password: String!): User!
+//     loginUser(email: String!, password: String!): User!
+//     logoutUser: Boolean!
+//   }
+
+//   schema {
+//     query: Query
+//     mutation: Mutation
+//   }
+// `;
+
+// module.exports = typeDefs;
+
+
+
+// ==============
+// comment out everything above this line to use J typedefs
+
+
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+type Query {
+  userSaveFile: Collection
+}
 
-  type Character {
-    character_id: Int!
-    searchable_name: String!
-    full_name: String!
-    role: String!
-    bio: String!
-  }
+type Collection {
+  _id: ID
+  userinfo: Userinfo
+  characters: Character
+}
+
+type Userinfo {
+  username: String
+  password: String
+}
+
+type Character {
+  abe: CharacterObject!
+  barf: CharacterObject!
+  hydra: CharacterObject!
+  shady: CharacterObject!
+  taylor: CharacterObject!
+  violet: CharacterObject!
+  zara: CharacterObject!
+}
+
+type CharacterObject {
+  detail: CharacterDetail
+  inventory: [InventoryItem]
+  wishlist: String
+}
+
+type CharacterDetail {
+  bio: String
+  name: String
+  role: String
+}
+
+type InventoryItem {
+  key: String
+  description: String
+  name: String
+}
+
+type InventoryContainer {
+  allItems: InventoryItem
+}
 
 
-  type Query {
-    test: String!
-    biography(searchableName: String!): Character!
-  }
 
-  type Item {
-    item_id: Int!
-    searchable_item: String!
-    item_name: String!
-    description: String!
-  }
 
-  type Query {
-    test: String!
-    item(searchableItem: String!): Item!
-    items: [Item]!
-  }
-
-  type Inventory {
-    full_name: Character!
-    item_name: Item!
-  }
-
-  type Query {
-    test: String!
-    inventoryByCharacter(character: String!): [Inventory]!
-    inventory: [Inventory]!
-  }
-
-  type Mutation {
-    trade(item1: String!, item2: String!): Inventory!
-  }
-
-  type User {
-    id: ID!
-    username: String!
-    email: String!
-    password: String!
-  }
-
-  type Mutation {
-    createUser(username: String!, email: String!, password: String!): User!
-    loginUser(email: String!, password: String!): User!
-    logoutUser: Boolean!
-  }
-
-  schema {
-    query: Query
-    mutation: Mutation
-  }
 `;
 
 module.exports = typeDefs;
