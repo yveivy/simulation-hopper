e8fef6813d10671b9b9a64e773e2d4c102ed1448
Valais Girl <Yevette@MacBook-Pro.local>
2023-06-13 10:08:02 -0500

    seed data and seeding functions

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,27 +1,31 @@
 const mongoose = require('mongoose');
-const seedCharacters = require('./characterData');
-const seedItems = require('./itemData');
-const seedInventory = require('/inventoryData');
+const { Characters, Items } = require('../models');
+
+const characterData = require('./characterData');
+const itemData = require('./itemData');
+const seedInventory = require('./seedInventory');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI, {
-  userNewUrlParser: true,
+
+console.log('MONGODB_URI:', process.env.MONGODB_URI);
+mongoose.connect('mongodb+srv://yveivy:Yevette1@classactivities.fq7zpnf.mongodb.net/gameDB', {
+  useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 const seedDatabase = async () => {
   try {
-    await seedCharacters();
+    await Characters.deleteMany({});
+    await Characters.insertMany(characterData);
 
-    await seedItems();
+    await Items.deleteMany({});
+    await Items.insertMany(itemData);
 
     await seedInventory();
 
     console.log('Database seeding completed successfully.');
   } catch (error) {
     console.error('Error seeding database:', error);
-  } finally {
-    mongoose.disconnect();
   }
 };
 
--- server/seeds/seedInventory.js
+++ server/seeds/seedInventory.js
@@ -1,33 +1,41 @@
-const { Inventory, Characters, Items } = require('../models');
-const inventoryData = require('./inventoryData.json');
+const { Inventory, Character, Item } = require('../models');
+const inventoryData = require('./inventoryData');
 
 const seedInventory = async () => {
     try {
         //Retrieve all characters and items from the database
 
-        const characters = await Characters.find({});
-        const items = await Items.find({});
+        const characters = await Character.find({});
+        const items = await Item.find({});
 
         //Map the character and item names to their corresponding object IDs
         const characterIdMap = {};
         const itemIdMap = {};
 
         characters.forEach((character) => {
-            characterIdMap[character.searchable_name] = character._id;
+            characterIdMap[character.searchable_name] = character.searchable_name;
         });
 
         items.forEach((item) => {
-            itemIdMap[item.searchable_item] = item._id;
+            itemIdMap[item.searchable_item] = item.searchable_item;
         });
+        //check for existing inventory entries
+       const existingInventory = await Inventory.find({});
+        //If entries exist, delete before seeding
+       if (existingInventory.length > 0) {
+        await Inventory.deleteMany({});
+        console.log('Existing inventory entries deleted.');
+       }
 
-        //Create inventory entries using the object IDs
+        //Create inventory entries using the character names 
 
         const inventorySeeds = inventoryData.map((entry) => ({
-            character:characterIdMap[entry.full_name],
-            item: itemIdMap[entry.item_name],
+            character:characterIdMap[entry.searchable_name],
+            item: itemIdMap[entry.searchable_item],
         }));
 
         //Insert the inventory seeds into the database 
+        await Inventory.deleteMany({});
         await Inventory.insertMany(inventorySeeds);
 
         console.log('Inventory seeding completed successfully.');
