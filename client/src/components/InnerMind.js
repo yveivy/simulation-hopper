d11703d9779334da04988530703706a6857f853b
Edward Wells <edward.wells87@gmail.com>
2023-06-16 10:39:39 -0500

    animation conditional but it still needs work

--- client/src/components/InnerMind.js
+++ client/src/components/InnerMind.js
@@ -2,37 +2,43 @@ import React, { useState, useEffect } from "react";
 import ShamanForGame from "../images/ShamanForGame.gif";
 import InnerMindGif from "../images/InnerMindGif.gif";
 
+// trying to implement logic for interaction object here
+import { resetInteractionGlobalVars } from "../utils/interactionMenu";
 const InnerMind = () => {
   const [firstGifClass, setFirstGifClass] = useState("invisible");
   const [secondGifClass, setSecondGifClass] = useState("invisible");
 
   useEffect(() => {
-    setFirstGifClass("visible");
-//add if statement for if interaction object is shaman THEN render the remaining code
+    if (window.interactionObject === "abe-harmony") {
+      console.log('he is interacting with the target')
+      setFirstGifClass("visible");
 
-    const firstGifTimeout = setTimeout(() => {
-      setFirstGifClass("invisible");
-      setSecondGifClass("visible");
-    }, 25000); // Hide the first GIF after 15 seconds and show the second GIF
+      const firstGifTimeout = setTimeout(() => {
+        setFirstGifClass("invisible");
+        setSecondGifClass("visible");
+      }, 25000); // Hide the first GIF after 15 seconds and show the second GIF
 
-    const secondGifTimeout = setTimeout(() => {
-      setSecondGifClass("invisible");
-    }, 30000);
-    
-    // this is where to add the functionality to make a keydown listener to end the shaman interaction
+      const secondGifTimeout = setTimeout(() => {
+        setSecondGifClass("invisible");
+      }, 30000);
 
-    return () => {
-      clearTimeout(firstGifTimeout);
-      clearTimeout(secondGifTimeout);
-    };
+      return () => {
+        clearTimeout(firstGifTimeout);
+        clearTimeout(secondGifTimeout);
+      };
+    }
   }, []);
 
-  return (
-    <div>
-      <img src={ShamanForGame} alt="ShamanGif" className={firstGifClass} />
-      <img src={InnerMindGif} alt="InnerMindGif" className={secondGifClass} />
-    </div>
-  );
+  if (window.interactionObject === "abe-harmony") {
+    return (
+      <div>
+        <img src={ShamanForGame} alt="ShamanGif" className={firstGifClass} />
+        <img src={InnerMindGif} alt="InnerMindGif" className={secondGifClass} />
+      </div>
+    );
+  } else {
+    return null; // Render nothing if the condition is not met
+  }
 };
 
 export default InnerMind;
