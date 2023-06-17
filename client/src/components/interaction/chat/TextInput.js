57b3f247badf2e17e850780871d24f54c46fd764
Valais Girl <Yevette@MacBook-Pro.local>
2023-06-16 21:31:38 -0500

    styled dialogue box and renamed headshot files

--- client/src/components/interaction/chat/TextInput.js
+++ client/src/components/interaction/chat/TextInput.js
@@ -12,25 +12,62 @@ export const TextInput = () => {
 
     const handleSubmit = async (event) => {
         
-        event.preventDefault();
-        addDialogue('Barf', inputText); // User's dialogue
-        //ToDO: choose prompt (based on user) 
-        // var prompt = "test prompt"
-        const npcResponse = await fetchOpenAiApi(inputText); // Some function that generates NPC response
-        addDialogue(interactionObject, npcResponse); // NPC's dialogue
-        console.log("TextInput.js dialogueList_______", dialogueList)
-        setInputText(''); // Clear the input field
+        try {
+            event.preventDefault();
+            addDialogue('Barf', inputText); // User's dialogue
+            //ToDO: choose prompt (based on user) 
+            // var prompt = "test prompt"
+            const npcResponse = await fetchOpenAiApi(inputText); // Some function that generates NPC response
+            addDialogue(interactionObject, npcResponse);//NPC's dialogue
+            console.log("TextInput.js dialogueList_______", dialogueList)
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setInputText(''); //Clear the input field
+        }
+     
     };
 
     return (
         <div id="text-input-container">
             <form id="chat-input-form" onSubmit={handleSubmit}>
-                <textarea
+                <textarea  style={{
+                    width: '100%',
+                    height: '25px',
+                    borderRadius: '5px',
+                    padding: '10px',
+                    fontSize: '16px',
+                    margin: '20 0px',
+                    backgroundColor: ' rgb(66, 133, 55)',
+                    color: 'white',
+                    
+                }}
                     id = "chat-input"
                     value={inputText}
                     onChange={(e) => setInputText(e.target.value)}
                 />
-                <button id="submit-chat-button">Submit</button>
+                <button style={{
+                    height: '45px',
+                    borderRadius: '5px',
+                    padding: '10px',
+                    fontSize: '16px',
+                    margin: '20 0px',
+                    backgroundColor: ' rgb(66, 133, 55)',
+                    color: 'white',
+                    border: 'none',
+                    boxShadow: '10px 10px 20px rgba(0,0,0,0.3)',
+                    transition: '0.3s ease' 
+                    
+                }} 
+                onMouseDown={(e) => {
+                    e.target.style.boxShadow = '2px 2px 5px rgba(99,157,82,0.2';
+                }}
+
+                onMouseUp={(e) => {
+                    e.target.style.boxShadow = '5px 5px 10px rgba(66,133,55, 0.3';
+                }}
+                
+                id="submit-chat-button">Submit</button>
             </form>
         </div>
     );
