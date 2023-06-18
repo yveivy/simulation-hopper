90c9f0035a5db5f14e08ca49e2d01b8c340b42a0
willcline613 <willrcline.atx@gmail.com>
2023-06-17 20:22:02 -0500

    taylor tuck ai calls integrated into if statement and useEffect in TextInput.js

--- client/src/components/interaction/chat/TextInput.js
+++ client/src/components/interaction/chat/TextInput.js
@@ -1,42 +1,67 @@
-import { useState, useContext } from 'react'
-import { createPromptForNpcResponseToChat, createPromptRobotResponseToChat, fetchOpenAiApi, formatDialogueForPrompt } from '../../../utils/ai';
+import { useState, useContext, useEffect } from 'react'
+import { createPremisePromptFor20Questions, createPromptForNpcResponseToChat, createPromptRobotResponseToChat, createResponsePromptFor20Questions, fetchOpenAiApi, formatDialogueForPrompt } from '../../../utils/ai';
 import { DialogueContext} from '../Interaction';
 import { fetchOneCharacterData } from '../../../utils/db/fetches';
 import "../../../css/overlay1.css"
 import { updateUserObjectives } from '../../../utils/inventory';
 
-export const TextInput = (spec) => {
-    // var interactionObject = window.interactionObject
-    // var interactionObject = "violet"
+export const TextInput = ({specialFeatures=false}) => {
+    var interactionObject = window.interactionObject
+    // var interactionObject = 'taylor'
 
     const { addDialogue, dialogueList } = useContext(DialogueContext);
     const [inputText, setInputText] = useState("");
 
+    const [secretWord, setSecretWord] = useState("")
+
+    useEffect(() => {
+        const setup20Questions = async () => {
+            console.log("specialFeatures_______", specialFeatures)
+            if (specialFeatures===true && interactionObject === "taylor" && dialogueList.length < 1) {
+                var secretWordString = await fetchOpenAiApi(createPremisePromptFor20Questions())
+                setSecretWord(secretWordString)
+                addDialogue("Taylor Tuck", "Haha I'm so glad you'll humor me. I'm so bored and love to make people play my game. I'm thinking of an animal. Go ahead and ask me questions and I'll respond with yes or no, or if you guess I'll tell you if it's correct.")
+            }
+        }
+        setup20Questions()
+    }, [addDialogue, setSecretWord, dialogueList.length, interactionObject, specialFeatures])
+  
+
     const handleSubmit = async (event) => {
-        var interactionObject = window.interactionObject
+        
         console.log("interactionObject_______", interactionObject)
         
         event.preventDefault();
         const localCopyOfDialogueList = [...dialogueList, {speaker: 'Barf', text: inputText}]; //local copy
-        addDialogue('Barf', inputText);
-        var chatHistory = formatDialogueForPrompt(localCopyOfDialogueList)
+        var chatHistory
         var npcFullName;
         var prompt;
-        if (interactionObject === "") {
-            // interactionObject = "robot"
-            updateUserObjectives()
-            prompt = createPromptRobotResponseToChat(chatHistory, window.globalVars.userObjectives)
-            npcFullName = "Robot"
+
+        if (specialFeatures === true) { 
+            if (interactionObject === "taylor") {
+                await addDialogue('Barf', inputText)
+                prompt = await createResponsePromptFor20Questions(secretWord, inputText)
+                npcFullName = "Taylor Tuck"
+            }
         } else {
-            var npcData = await fetchOneCharacterData(interactionObject)
-            console.log(`npcData for ${interactionObject}_____________`, npcData)
-            prompt = createPromptForNpcResponseToChat(npcData.full_name, npcData.bio, npcData.role, chatHistory)
-            npcFullName = npcData.full_name
+            addDialogue('Barf', inputText);
+            chatHistory = formatDialogueForPrompt(localCopyOfDialogueList)
+            if (interactionObject === "") {
+                updateUserObjectives()
+                prompt = createPromptRobotResponseToChat(chatHistory, window.globalVars.userObjectives)
+                npcFullName = "Robot"
+            } else {
+                var npcData = await fetchOneCharacterData(interactionObject)
+                console.log(`npcData for ${interactionObject}_____________`, npcData)
+                prompt = createPromptForNpcResponseToChat(npcData.full_name, npcData.bio, npcData.role, chatHistory)
+                npcFullName = npcData.full_name
+            }
         }
         const npcResponse = await fetchOpenAiApi(prompt); // Some function that generates NPC response
         addDialogue(npcFullName, npcResponse); // NPC's dialogue
         console.log("TextInput.js dialogueList_______", localCopyOfDialogueList)
         setInputText(''); // Clear the input field
+
     };
 
     const handleTextAreaKeyDown = (event) => {
