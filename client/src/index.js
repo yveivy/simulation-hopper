9ca23f6a0c22fd3df9c57f4775bfe6014708c799
willcline613 <willrcline.atx@gmail.com>
2023-06-20 14:00:47 -0500

    comment test

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,12 @@ import App from './App';
 import GameOverlay from "./GameOverlay"
 
 //Homepage
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  ReactDOM.render(<App />, rootElement);
-}
-
-// const overlayElement = document.getElementById('game-overlay-react-root');
-// if (overlayElement) {
-//   ReactDOM.render(<GameOverlay />, overlayElement);
+// const rootElement = document.getElementById('root');
+// if (rootElement) {
+//   ReactDOM.render(<App />, rootElement);
 // }
+
+const overlayElement = document.getElementById('game-overlay-react-root');
+if (overlayElement) {
+  ReactDOM.render(<GameOverlay />, overlayElement);
+}
